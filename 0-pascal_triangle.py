ee433535039da9e39e655d62de1d6e8b464798e5
Lamber22 <Lamberjoe5@gmail.com>
2024-04-30 04:43:03 +0000

    Returns a Pascal’s triangle of n

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def pascal_triangle(n):
+    """Create a pascal_triangle function that returns a list of lists
+    of integers representing the Pascal’s triangle of n
+    """
+    if n <= 0:
+        return []
+    res = []
+    if n > 0:
+        for i in range(1, n + 1):
+            level = []
+            C = 1
+            for j in range(1, i + 1):
+                level.append(C)
+                C = C * (i - j) // j
+            res.append(level)
+    return res
